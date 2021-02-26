1853f92cf0dbbccbe0ce39abc033c458e624023a
LuisEliasTec <luiselias_1@hotmail.com>
2021-02-25 22:57:33 -0700

    First commit in my personal repository

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,43 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I',m John</h1>
+            <h4>Freelance web and mobile UI/UX Designer</h4>
+            <Link to="/contact" className="btn">
+              Contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
